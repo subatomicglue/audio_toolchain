bb4ded7477276ef01f741d1078decd79ad0228ec
km <kmeinert@aviahealthinnovation.com>
2020-12-11 19:02:03 -0600

    fix this for directory prefix

--- sf2_to_sfz.sh
+++ sf2_to_sfz.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 infile=$1
-outfile=$(echo "$infile" | cut -f 1 -d '.')
+outfile=$(echo "$infile" | sed -E "s/\.[^.]+$//g")
 
 /Applications/polyphone-2.2.app/Contents/MacOS/polyphone -3 -i "$infile" -d . -o "${outfile}_sfz"
-
-
-
