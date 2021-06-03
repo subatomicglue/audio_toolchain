f1ca38ed3abb75eb4242fb8c320d932cb7ba11d6
km <kmeinert@aviahealthinnovation.com>
2021-06-03 15:37:11 -0500

    improve documentation a bit

--- batch_convert/examples/catalog.sh
+++ batch_convert/examples/catalog.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# copy this file to your music catalog, edit the actions for your albums
+
 ################################################################################
-# -->> TODO: delete this section!
+# -->> TODO: DELETE THIS SECTION!
 # just for demo purposes, create the test data if it doesn't exist yet
 [ ! -f "crunchy/subatomicglue - crunchy - 01 - heartonfire.wav" ] && cd crunchy && ./create_test_data.sh && cd -
 [ ! -f "inertial/subatomicglue - inertialdecay - 01 - hard.wav" ] && cd inertial && ./create_test_data.sh && cd -
 [ ! -f "selling/subatomicglue-selling your friend for cash-01-bospherous.wav" ] && cd selling && ./create_test_data.sh && cd -
 [ ! -f "spinning/subatomicglue - spinningtrees - 01 - start.wav" ] && cd spinning && ./create_test_data.sh && cd -
-# -->> TODO: delete this section!
+# -->> TODO: DELETE THIS SECTION!
 ################################################################################
 
 SRCDIR="`pwd`"
