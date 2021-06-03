8ddc01715aaac3441cd9d8b1815eafad3c896c93
km <kmeinert@aviahealthinnovation.com>
2021-06-03 15:07:13 -0500

    fill out depends.sh to check them all

--- depends.sh
+++ depends.sh
@@ -3,19 +3,51 @@
 shopt -s expand_aliases
 alias ldd='otool -L'
 
-echo "==========================="
-echo "here's a list dependencies:"
-echo "==========================="
+echo "=============================="
+echo "sanity"
+echo "=============================="
+
+echo "sed should be GNU sed:"
+sed --version | grep "GNU sed"
+
+echo "=============================="
+echo "sampler tools dependencies:"
+echo "=============================="
 
 echo `which bash`
-
-echo `which perl`
-
 echo `which node`
-
 echo `which sox`
+echo `ls /Applications/polyphone-*.app/Contents/MacOS/*`
+
+echo "=============================="
+echo "batch tools dependencies:"
+echo "=============================="
+
+echo `which bash`
+echo `which perl`
+echo `which flac`
+echo `which lame`
+echo `which vorbis-tools`
+echo `which sox`
+echo `which fdk-aac-encoder`
+echo `which faac`
+echo `which atomicparsley`
+echo `which imagemagick`
+echo `which sip`
+
+
+echo ""
+echo "=========================================================="
+echo "verbose stuff"
+echo "=========================================================="
+
+echo ""
+echo "what's sox linked to:"
+echo ""
 ldd `which sox`
 
-echo `ls /Applications/polyphone-*.app/Contents/MacOS/*`
+echo ""
+echo "what's polyphone linked to?"
+echo ""
 ldd `ls /Applications/polyphone-*.app/Contents/MacOS/*`
 
